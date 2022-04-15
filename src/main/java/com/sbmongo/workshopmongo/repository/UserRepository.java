3a69718f6972a08a8377897c7d72079ce5b5db69
Idevaldo <inetomec@yahoo.com.br>
2022-04-15 19:31:12 -0300

    feat: :sparkles: Connecting to MongoDB with Repository and Service

---
+++ src/main/java/com/sbmongo/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sbmongo.workshopmongo.repository;
+
+import com.sbmongo.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
