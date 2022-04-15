60bcb7c60a242f3dbc37b72fbe132016b88be0f4
Idevaldo <inetomec@yahoo.com.br>
2022-04-15 13:40:26 -0300

    feat: :sparkles: First commit

---
+++ src/test/java/com/sbmongo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbmongo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
